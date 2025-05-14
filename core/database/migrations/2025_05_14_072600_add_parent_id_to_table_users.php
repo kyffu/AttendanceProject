c1e9d608a59584ab6558463e623807d29621a597
MasPujuhhh <puguhaji02@gmail.com>
2025-05-14 22:33:58 +0700

    fix

--- core/database/migrations/2025_05_14_072600_add_parent_id_to_table_users.php
+++ core/database/migrations/2025_05_14_072600_add_parent_id_to_table_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreign('parent_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('parent_id')->references('id')->on('users')->onDelete('cascade')->nullable();
         });
     }
 
